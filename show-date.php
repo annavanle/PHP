c4dd1080e7e2d1a39a887ad27541bde905f77428
Van Le <71357877+annavanle@users.noreply.github.com>
2020-10-13 11:44:59 +0900

    Add files via upload

---
+++ show-date.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+// Xuất dữ liệu echo 
+$a=100;
+echo "Giá trị của a:".$a;
+
+// Xuất dự liệu mảng
+$my_array = array('A','B','C');
+echo "<pre>";
+Print_r($my_array);
+echo "</pre>";
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=11, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <style>
+        h1{
+            color:red;
+        }
+    </style>
+<h1>Hello <?php echo " Vân Lê" ?></h1>    
+</body>
+</html>
